c35b1ff15f42033f4849c6ff37f2210768d973d6
antonyni <60332071+antonyni@users.noreply.github.com>
2023-02-01 12:57:46 -0500

    v.14

--- pages/index.js
+++ pages/index.js
@@ -117,7 +117,7 @@ export default function Home() {
               
             </div>
             <div className ="flex flex-col  dark:bg-gray-800 bg-blue-200  mb-32 rounded-xl p-6 w-1/5 dark:text-blue-300">
-              <p className="text-2xl  border-b-4 dark:border-gray-900 mb-4"> Tools/Concepts</p>
+              <p className="text-2xl  border-b-4 dark:border-gray-900 mb-4"> Tools / Concepts</p>
               <p> Git</p>
               <p> Bash</p>
               <p> VS Code</p>
