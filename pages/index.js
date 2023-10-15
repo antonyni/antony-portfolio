e504b07530d8ef0756ac615756e4b5748c0cfe7c
Antony <antonyni1010@gmail.com>
2023-10-15 13:25:12 -0400

    refactored the text scramble code to make it a function instead of a class for legibility

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
   useEffect(() => {
     if (!done) {
       const el = document.querySelector('.text');
-      const fx = new TextScramble(el);
+      const fx = textScramble(el);
       let counter = 0
 
       const next = () => {
@@ -225,58 +225,66 @@ export default function Home() {
   )
 }
 
-class TextScramble {
-  constructor(el) {
-    this.el = el
-    this.chars = '!<>-_\\/[]{}—=+*^?#________'
-    this.update = this.update.bind(this)
-  }
-  setText(newText) {
-    const oldText = this.el.innerText
-    const length = Math.max(oldText.length, newText.length)
-    const promise = new Promise((resolve) => this.resolve = resolve)
-    this.queue = []
-    for (let i = 0; i < length; i++) {
-      const from = oldText[i] || ''
-      const to = newText[i] || ''
-      const start = Math.floor(Math.random() * 40)
-      const end = start + Math.floor(Math.random() * 40)
-      this.queue.push({ from, to, start, end })
-    }
-    cancelAnimationFrame(this.frameRequest)
-    this.frame = 0
-    this.update()
-    return promise
-  }
-  update() {
-    let output = ''
-    let complete = 0
-    for (let i = 0, n = this.queue.length; i < n; i++) {
-      let { from, to, start, end, char } = this.queue[i]
-      if (this.frame >= end) {
-        complete++
-        output += to
-      } else if (this.frame >= start) {
+function textScramble(element) {
+  const chars = '!<>-_\\/[]{}—=+*^?#________';
+
+  let frame = 0;
+  let frameRequest;
+  let queue = [];
+  let resolve;
+
+  function update() {
+    let output = '';
+    let complete = 0;
+    for (let i = 0, n = queue.length; i < n; i++) {
+      let { from, to, start, end, char } = queue[i];
+      if (frame >= end) {
+        complete++;
+        output += to;
+      } else if (frame >= start) {
         if (!char || Math.random() < 0.28) {
-          char = this.randomChar()
-          this.queue[i].char = char
+          char = randomChar();
+          queue[i].char = char;
         }
-        output += `<span class="dud">${char}</span>`
+        output += `<span class="dud">${char}</span>`;
       } else {
-        output += from
+        output += from;
       }
     }
-    this.el.innerHTML = output
-    if (complete === this.queue.length) {
-      this.resolve()
+    element.innerHTML = output;
+    if (complete === queue.length) {
+      resolve();
     } else {
-      this.frameRequest = requestAnimationFrame(this.update)
-      this.frame++
+      frameRequest = requestAnimationFrame(update);
+      frame++;
     }
   }
-  randomChar() {
-    return this.chars[Math.floor(Math.random() * this.chars.length)]
+
+  function setText(newText) {
+    const oldText = element.innerText;
+    const length = Math.max(oldText.length, newText.length);
+    const promise = new Promise((res) => (resolve = res));
+    queue = [];
+    for (let i = 0; i < length; i++) {
+      const from = oldText[i] || '';
+      const to = newText[i] || '';
+      const start = Math.floor(Math.random() * 15);
+      const end = start + Math.floor(Math.random() * 15);
+      queue.push({ from, to, start, end });
+    }
+    cancelAnimationFrame(frameRequest);
+    frame = 0;
+    update();
+    return promise;
   }
+
+  function randomChar() {
+    return chars[Math.floor(Math.random() * chars.length)];
+  }
+
+  return {
+    setText,
+  };
 }
 
 const phrases = [
